3c0089346b969398f42723c661b5ed9b82ec5c8c
TheRealCasadaro <TheRealCasadaro@TheRealCasadaro.com>
2019-02-04 18:24:28 -0800

    made it so the project name apeares in green after the project has been created

--- create-webproject.psm1
+++ create-webproject.psm1
@@ -1,12 +1,29 @@
 function create-webProject($projectName) {
-  if(!($projectName)) {
-    write-host 'Project name Required'
-    write-host 'enter project name'
-    $projectName = read-host
+  
+  $thisProject = $projectName
+  Write-Host $thisPRoject 
+  
+  if(!($thisProject)) {
+    write-host 'Project name Required' ;
+    write-host 'enter project name' ;
+    $thisProject = read-host;
   }
 
-  mkdir $projectName, $projectName/js, $projectName/img, $projectName/css
-  new-item $projectName/js/app.js
-  new-item $projectName/css/style.css
+  mkdir $thisProject, $thisProject/js, $thisProject/img, $thisProject/css;
+  new-item $thisProject/js/app.js ;
+  new-item $thisProject/css/style.css ;
+  new-item $thisProject/index.html ;
+ 
 
-}
+  Write-Output 'start VSCODE?'
+  Write-Output '[Y] for yes, [N] for no'
+
+  $work = read-host
+  if($work -eq 'Y' -or $work -eq 'y') {
+    code $thisProject
+  } else {
+    Write-Host "All done! " -ForegroundColor White -NoNewline; 
+    Write-Host "$thisProject " -ForegroundColor Green -NoNewline; 
+    Write-Host "project has been Created! " -ForegroundColor White -NoNewline;
+  }
+};
